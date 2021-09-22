8260bb859c6a50a9f3a9b700e02953272c90c515
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-22 09:54:29 -0400

    Create signup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+
 function Signup() {
-    return null;
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        password_confirmation: ""
+    });
+
+    function handleFormChange(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        };
+        fetch("/signup", configObj)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(errors => console.log(errors))
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="first_name" onChange={handleFormChange} placeholder="First name" />
+            <input type="text" name="last_name" onChange={handleFormChange} placeholder="Last name" />
+            <input type="email" name="email" onChange={handleFormChange} placeholder="Email address" />
+            <input type="password" name="password" onChange={handleFormChange} placeholder="Password" />
+            <input type="password" name="password_confirmation" onChange={handleFormChange} placeholder="Confirm password" />
+            <input type="submit" />
+        </form>
+    );
 };
 
 export default Signup;
