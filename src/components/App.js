e57022881adee7bd0c84db5e9b06280aefb2abef
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-22 14:13:05 -0400

    Add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Login from './Login';
 import Signup from './Signup';
 
 function App() {
     return (
-        <>
-            <Login />
-            <Signup />
-        </>
+        <Router>
+            <Switch>
+                <Route exact path="/">Hello, World!</Route>
+                <Route exact path="/login"><Login /></Route>
+                <Route exact path="/signup"><Signup /></Route>
+            </Switch>
+        </Router>
     );
 };
 
