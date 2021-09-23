3e9cd441fa6955b8074cff57734a18c7e358cc39
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 12:11:34 -0400

    Add receiptsSlice to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './userSlice';
+import receiptsSlice from './receiptsSlice';
 
 const store = configureStore({
-    reducer: { user: userSlice.reducer },
+    reducer: {
+        user: userSlice.reducer,
+        receipts: receiptsSlice.reducer
+    },
 })
 
 export default store;
