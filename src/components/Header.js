41aedaa9187f68236b4f94c95d83a303e0623dcb
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 08:46:39 -0400

    Add login/logout/signup buttons in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
-function Header() {
+import { Link } from 'react-router-dom';
+import CSRFToken from './cookies';
+
+function Header({ user, onLogout }) {
+    function handleLogout() {
+        fetch("/logout", { method: "DELETE", headers: { "X-CSRF-Token": CSRFToken(document.cookie) } })
+            .then(() => {
+                onLogout()
+            });
+    };
+    
     return (
         <nav>
-            HSA Tracker
+            <Link to="/">HSA Tracker</Link>
+            {user ? 
+                <button onClick={handleLogout}>Log Out</button>
+            : <>
+                <button>
+                    <Link to="/login">Login</Link>
+                </button>
+                <button>
+                    <Link to="/signup">Signup</Link>
+                </button></>
+            }
         </nav>
     );
 };
