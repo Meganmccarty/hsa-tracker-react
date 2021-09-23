a91f2f178fb8473ece8b7fdb0434ab5efda464a5
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 12:11:54 -0400

    Display receipt records in console

--- src/components/ReceiptRecordsList.js
+++ src/components/ReceiptRecordsList.js
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { retrieveReceipts } from '../store/receiptsSlice';
+
 function ReceiptRecordsList() {
+    const dispatch = useDispatch();
+    const receipts = useSelector(state => state.receipts.receipts);
+    
+    useEffect(() => {
+        dispatch(retrieveReceipts());
+    }, [dispatch]);
+
+    console.log(receipts);
+
     return <h1>Receipt Records List</h1>
 };
 
