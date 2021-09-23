d5bedb60bba6deaea179efdb810b6c23cb13c25c
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 14:36:51 -0400

    Display receipt records as table

--- src/components/ReceiptRecordsList.js
+++ src/components/ReceiptRecordsList.js
@@ -12,7 +12,39 @@ function ReceiptRecordsList() {
 
     console.log(receipts);
 
-    return <h1>Receipt Records List</h1>
+    const displayReceipts = receipts.map(receipt => {
+        return (
+            <tr key={receipt.id}>
+                <td>{receipt.trans_date}</td>
+                <td>{receipt.provider}</td>
+                <td>{receipt.decription}</td>
+                <td>{receipt.amount}</td>
+                <td>{receipt.payment_method}</td>
+                <td>{receipt.reimbursed}</td>
+            </tr>
+        )
+    })
+
+    return (
+        <>
+            <h1>Receipt Records List</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Provider</th>
+                        <th>Description</th>
+                        <th>Amount</th>
+                        <th>Payment Method</th>
+                        <th>Reimbursed?</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {displayReceipts}
+                </tbody>
+            </table>
+        </>
+    );
 };
 
 export default ReceiptRecordsList;
