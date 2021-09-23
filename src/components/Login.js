a7eef73647e1e24702df9159b8cbacb3b491e16d
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 08:48:20 -0400

    Create login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,39 @@
-function Login() {
-    return null;
+import { useState } from 'react';
+import CSRFToken from './cookies';
+
+function Login({ onLogin }) {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    function handleFormChange(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": CSRFToken(document.cookie)
+            },
+            body: JSON.stringify(formData)
+        };
+        fetch("/login", configObj)
+            .then(response => response.json())
+            .then(data => onLogin(data))
+            .catch(errors => console.log(errors))
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="email" name="email" onChange={handleFormChange} placeholder="Email address" />
+            <input type="password" name="password" onChange={handleFormChange} placeholder="Password" />
+            <input type="submit" />
+        </form>
+    );
 };
 
 export default Login;
