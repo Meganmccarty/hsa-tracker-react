ccd9e1a0b59f9714c2df58daf7fa1b0cde735c3c
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 09:22:41 -0400

    Create thunk for user login

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -2,7 +2,20 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import CSRFToken from '../components/cookies';
 
 export const createUser = createAsyncThunk("user/createUser", async (formData) => {
-    const response = await fetch ("/signup", {
+    const response = await fetch("/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": CSRFToken(document.cookie)
+        },
+        body: JSON.stringify(formData)
+    });
+    const data = await response.json();
+    return data;
+});
+
+export const onLogin = createAsyncThunk("user/onLogin", async (formData) => {
+    const response = await fetch("/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -47,6 +60,26 @@ const userSlice = createSlice({
             }
         },
         [createUser.rejected](state, action) {
+            state.status = "rejected";
+            if (action.payload) {
+                state.errors = action.payload.errorMessages;
+            } else {
+                state.errors = action.error.message;
+            }
+        },
+        [onLogin.pending](state) {
+            state.status = "loading";
+        },
+        [onLogin.fulfilled](state, action) {
+            state.status = "completed";
+            if (action.payload.errors) {
+                state.errors = action.payload.errors;
+            } else {
+                state.user = action.payload;
+                state.errors = [];
+            }
+        },
+        [onLogin.rejected](state, action) {
             state.status = "rejected";
             if (action.payload) {
                 state.errors = action.payload.errorMessages;
