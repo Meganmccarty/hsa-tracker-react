1729beb3076be109a74fce52ef35bcabcb9a84f3
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-24 19:08:02 -0400

    Add thunk for creating receipt, remove commented-out code

--- src/store/receiptsSlice.js
+++ src/store/receiptsSlice.js
@@ -1,17 +1,18 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import CSRFToken from '../components/cookies';
 
-// export const retrieveReceipts = createAsyncThunk("receipts/retrieveReceipts", async () => {
-//     const response = await fetch("/receipt-records");
-//     const data = await response.json();
-//     return data;
-// })
-
-// export const retrieveOneReceipt = createAsyncThunk("receipts/retrieveOneReceipt", async (id) => {
-//     const response = await fetch(`/receipt-records/${id}`);
-//     const data = await response.json();
-//     return data;
-// })
+export const createReceipt = createAsyncThunk("receipts/createReceipt", async (formData) => {
+    const response = await fetch("/receipt-records", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": CSRFToken(document.cookie)
+        },
+        body: JSON.stringify(formData)
+    });
+    const data = await response.json();
+    return data;
+});
 
 
 const initialState = {
@@ -42,46 +43,26 @@ const receiptsSlice = createSlice({
         }
     },
     extraReducers: {
-        // [retrieveReceipts.pending](state) {
-        //     state.status = "loading";
-        // },
-        // [retrieveReceipts.fulfilled](state, action) {
-        //     state.status = "fulfilled";
-        //     if (action.payload.errors) {
-        //         state.errors = action.payload.errors;
-        //     } else {
-        //         state.receiptList = action.payload;
-        //         state.errors = [];
-        //     }
-        // },
-        // [retrieveReceipts.rejected](state, action) {
-        //     state.status = "rejected";
-        //     if (action.payload) {
-        //         state.errors = action.payload.errorMessages;
-        //     } else {
-        //         state.errors = action.error.message;
-        //     }
-        // },
-        // [retrieveOneReceipt.pending](state) {
-        //     state.status = "loading";
-        // },
-        // [retrieveOneReceipt.fulfilled](state, action) {
-        //     state.status = "fulfilled";
-        //     if (action.payload.errors) {
-        //         state.errors = action.payload.errors;
-        //     } else {
-        //         state.receipt = action.payload;
-        //         state.errors = [];
-        //     }
-        // },
-        // [retrieveOneReceipt.rejected](state, action) {
-        //     state.status = "rejected";
-        //     if (action.payload) {
-        //         state.errors = action.payload.errorMessages;
-        //     } else {
-        //         state.errors = action.error.message;
-        //     }
-        // }
+        [createReceipt.pending](state) {
+            state.status = "loading";
+        },
+        [createReceipt.fulfilled](state, action) {
+            state.status = "completed";
+            if (action.payload.errors) {
+                state.errors = action.payload.errors;
+            } else {
+                state.receiptList.push(action.payload);
+                state.errors = [];
+            }
+        },
+        [createReceipt.rejected](state, action) {
+            state.status = "rejected";
+            if (action.payload) {
+                state.errors = action.payload.errorMessages;
+            } else {
+                state.errors = action.error.message;
+            }
+        }
     }
 })
 
