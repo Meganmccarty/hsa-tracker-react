e02a4a7a106bc71df5381134202323e65f624c7b
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-24 11:00:10 -0400

    Create new reducers, comment out get fetch request for all receipts

--- src/store/receiptsSlice.js
+++ src/store/receiptsSlice.js
@@ -1,11 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import CSRFToken from '../components/cookies';
 
-export const retrieveReceipts = createAsyncThunk("receipts/retrieveReceipts", async () => {
-    const response = await fetch("/receipt-records");
-    const data = await response.json();
-    return data;
-})
+// export const retrieveReceipts = createAsyncThunk("receipts/retrieveReceipts", async () => {
+//     const response = await fetch("/receipt-records");
+//     const data = await response.json();
+//     return data;
+// })
 
 export const retrieveOneReceipt = createAsyncThunk("receipts/retrieveOneReceipt", async (id) => {
     const response = await fetch(`/receipt-records/${id}`);
@@ -26,31 +26,37 @@ const receiptsSlice = createSlice({
     name: 'receipts',
     initialState,
     reducers: {
+        addUserReceipts(state, action) {
+            state.receiptList = action.payload
+        },
+        clearUserReceipts(state) {
+            state.receiptLits = [];
+        },
         toggleLoading(state, action) {
             state.loading = action.payload
         }
     },
     extraReducers: {
-        [retrieveReceipts.pending](state) {
-            state.status = "loading";
-        },
-        [retrieveReceipts.fulfilled](state, action) {
-            state.status = "fulfilled";
-            if (action.payload.errors) {
-                state.errors = action.payload.errors;
-            } else {
-                state.receiptList = action.payload;
-                state.errors = [];
-            }
-        },
-        [retrieveReceipts.rejected](state, action) {
-            state.status = "rejected";
-            if (action.payload) {
-                state.errors = action.payload.errorMessages;
-            } else {
-                state.errors = action.error.message;
-            }
-        },
+        // [retrieveReceipts.pending](state) {
+        //     state.status = "loading";
+        // },
+        // [retrieveReceipts.fulfilled](state, action) {
+        //     state.status = "fulfilled";
+        //     if (action.payload.errors) {
+        //         state.errors = action.payload.errors;
+        //     } else {
+        //         state.receiptList = action.payload;
+        //         state.errors = [];
+        //     }
+        // },
+        // [retrieveReceipts.rejected](state, action) {
+        //     state.status = "rejected";
+        //     if (action.payload) {
+        //         state.errors = action.payload.errorMessages;
+        //     } else {
+        //         state.errors = action.error.message;
+        //     }
+        // },
         [retrieveOneReceipt.pending](state) {
             state.status = "loading";
         },
