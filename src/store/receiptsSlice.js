9d4764b1ac116a92b3e7cef2ea84e0b760c25a63
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-24 20:18:46 -0400

    Remove thunk, add createReceipt reducer

--- src/store/receiptsSlice.js
+++ src/store/receiptsSlice.js
@@ -1,19 +1,4 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import CSRFToken from '../components/cookies';
-
-export const createReceipt = createAsyncThunk("receipts/createReceipt", async (formData) => {
-    const response = await fetch("/receipt-records", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-Token": CSRFToken(document.cookie)
-        },
-        body: JSON.stringify(formData)
-    });
-    const data = await response.json();
-    return data;
-});
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     receiptList: [],
@@ -38,6 +23,15 @@ const receiptsSlice = createSlice({
             const id = action.payload;
             state.receipt = state.receiptList.find(receipt => receipt.id === id);
         },
+        createReceipt(state, action) {
+            if (action.payload.errors) {
+                state.errors = action.payload.errors
+            } else {
+                state.receiptList.push(action.payload)
+                state.receipt = action.payload
+                state.errors = [];
+            }
+        },
         deleteReceipt(state, action) {
             const id = action.payload;
             state.receiptList = state.receiptList.filter(receipt => receipt.id !== id);
@@ -45,28 +39,6 @@ const receiptsSlice = createSlice({
         },
         toggleLoading(state, action) {
             state.loading = action.payload
-        }
-    },
-    extraReducers: {
-        [createReceipt.pending](state) {
-            state.status = "loading";
-        },
-        [createReceipt.fulfilled](state, action) {
-            state.status = "completed";
-            if (action.payload.errors) {
-                state.errors = action.payload.errors;
-            } else {
-                state.receiptList.push(action.payload);
-                state.errors = [];
-            }
-        },
-        [createReceipt.rejected](state, action) {
-            state.status = "rejected";
-            if (action.payload) {
-                state.errors = action.payload.errorMessages;
-            } else {
-                state.errors = action.error.message;
-            }
         }
     }
 })
