25085460460d31a87e0b2f7102edac4d8c30161b
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-01 15:48:07 -0400

    Add buttons for editing profile and changing password

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function Profile() {
     const user = useSelector(state => state.user.user);
 
-    return <h1>Profile Page for {user.first_name} {user.last_name}</h1>
+    return (
+        <>
+            <h1>Profile Page for {user.first_name} {user.last_name}</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td>{user.first_name} {user.last_name}</td>
+                    </tr>
+                    <tr>
+                        <td>Email Address</td>
+                        <td>{user.email}</td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>********</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button><Link to={`/profile/${user.id}/edit`}>Edit Profile</Link></button>
+            <button><Link to={`/profile/${user.id}/change-password`}>Change Password</Link></button>
+        </>
+    )
 };
 
 export default Profile;
