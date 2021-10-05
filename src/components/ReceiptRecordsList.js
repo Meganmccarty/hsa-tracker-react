27bae76e0fcde303e17ca131c4563eef52d30bbb
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-05 11:44:02 -0400

    Add expense totals for different categories

--- src/components/ReceiptRecordsList.js
+++ src/components/ReceiptRecordsList.js
@@ -43,6 +43,7 @@ function ReceiptRecordsList() {
                         <td><Link to={`/receipt-records/${receipt.id}`}>{receipt.trans_date}</Link></td>
                         <td>{receipt.provider}</td>
                         <td>{receipt.decription}</td>
+                        <td>{receipt.qualified_exp}</td>
                         <td>{parseFloat(receipt.amount).toFixed(2)}</td>
                         <td>{receipt.payment_method}</td>
                         <td>{receipt.reimbursed}</td>
@@ -51,7 +52,45 @@ function ReceiptRecordsList() {
             })
         }
     }
-    
+
+    const qualifiedExpenses = totalQualifiedExpenses();
+    const reimbursedExpenses = totalReimbursedExpenses();
+    const HSAExpenses = totalHSAExpenses();
+
+    function sumTotals(array) {
+        let expenses = 0;
+        array.map(element => {
+            return expenses += element.amount
+        })
+        return expenses
+    }
+
+    function totalQualifiedExpenses() {
+        if (receipts) {
+            const qualifiedReceipts = receipts.filter(receipt => {
+                return receipt.qualified_exp === 'Yes' ? receipt : null
+            })
+            return sumTotals(qualifiedReceipts)
+        }
+    }
+
+    function totalReimbursedExpenses() {
+        if (receipts) {
+            const reimbursedReceipts = receipts.filter(receipt => {
+                return receipt.reimbursed === 'Yes' ? receipt : null
+            })
+            return sumTotals(reimbursedReceipts)
+        }
+    }
+
+    function totalHSAExpenses() {
+        if (receipts) {
+            const HSAReceipts = receipts.filter(receipt => {
+                return receipt.payment_method === 'HSA Debit Card' ? receipt : null
+            })
+            return sumTotals(HSAReceipts)
+        }
+    }
 
     return (
         <>
@@ -61,12 +100,22 @@ function ReceiptRecordsList() {
             {loading ?
                 <Loading />
                 :
+                <>
+                <h3>Total Qualified Expenses</h3>
+                {parseFloat(qualifiedExpenses).toFixed(2)}
+                <h3>Total Reimbursed Expenses</h3>
+                {parseFloat(reimbursedExpenses).toFixed(2)}
+                <h3>Total Expenses Paid with HSA Card</h3>
+                {parseFloat(HSAExpenses).toFixed(2)}
+                <h3>Remaining Safe Amount to Withdraw from HSA</h3>
+                {parseFloat(qualifiedExpenses - reimbursedExpenses - HSAExpenses).toFixed(2)}
                 <table>
                     <thead>
                         <tr>
                             <th>Date</th>
                             <th>Provider</th>
                             <th>Description</th>
+                            <th>Qualified Expense?</th>
                             <th>Amount</th>
                             <th>Payment Method</th>
                             <th>Reimbursed?</th>
@@ -76,6 +125,7 @@ function ReceiptRecordsList() {
                         {displayReceipts()}
                     </tbody>
                 </table>
+                </>
             }
         </>
     );
