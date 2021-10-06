ceae83983e97ba7dcfb3a2f30d2c8f9f24b9d173
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-06 11:39:34 -0400

    Add css for header, minor formatting in js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
+import { Link, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { userActions } from '../store/userSlice';
 import { receiptActions } from '../store/receiptSlice';
-
-import { Link, useHistory } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
     const history = useHistory();
@@ -15,14 +15,14 @@ function Header() {
         fetch("/logout", {
             method: "DELETE",
         })
-        .then(response => response.json())
-        .then(data => {
-            localStorage.removeItem("token");
-            dispatch(receiptActions.clearReceipts())
-            dispatch(userActions.setMessage(data.status.message))
-            dispatch(userActions.userLogout())
-            history.push("/login")
-        })
+            .then(response => response.json())
+            .then(data => {
+                localStorage.removeItem("token");
+                dispatch(receiptActions.clearReceipts())
+                dispatch(userActions.setMessage(data.status.message))
+                dispatch(userActions.userLogout())
+                history.push("/login")
+            })
     };
 
     function handleMessages() {
@@ -40,34 +40,37 @@ function Header() {
         }
     }
 
+    function toggleNavBar(e) {
+        const nav = e.target.parentElement.parentElement;
+        if (nav.className === "navbar") {
+            nav.classList.add("responsive")
+        } else {
+            nav.className = "navbar"
+        }
+    }
+
     return (
-        <nav>
-            {handleMessages()}
-            <Link to="/">HSA Tracker</Link>
-            {user ?
-                <>
-                    <button>
+        <>
+            <nav className="navbar">
+                <Link to="/">HSA Tracker</Link>
+                {user ?
+                    <>
                         <Link to="/receipt-records">My Receipt Records</Link>
-                    </button>
-                    <button>
                         <Link to="/receipt-records/new">Add Receipt</Link>
-                    </button>
-                    <button>
                         <Link to="/profile">Profile</Link>
-                    </button>
-                    <button onClick={handleLogout}>Log Out</button>
-                </>
-                :
-                <>
-                    <button>
+                        <Link to="#" onClick={handleLogout}>Log Out</Link>
+                        <Link to="#" className="icon" onClick={toggleNavBar}><i className="fa fa-bars"></i></Link>
+                    </>
+                    :
+                    <>
                         <Link to="/login">Login</Link>
-                    </button>
-                    <button>
                         <Link to="/signup">Signup</Link>
-                    </button>
-                </>
-            }
-        </nav>
+                        <Link to="#" className="icon" onClick={toggleNavBar}><i className="fa fa-bars"></i></Link>
+                    </>
+                }
+            </nav>
+            {handleMessages()}
+        </>
     );
 };
 
