0b8fa241afc8aee4b715e77d72758053d4a94312
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-07 14:26:51 -0400

    Restructure profile, add more styling

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -17,28 +17,28 @@ function Profile() {
     }, [dispatch]);
 
     return (
-        <>
-            <h1>Profile Page for {user.first_name} {user.last_name}</h1>
-            {message ? <div id="message">{message}</div> : null}
-            <table>
-                <tbody>
-                    <tr>
-                        <td>Name</td>
-                        <td>{user.first_name} {user.last_name}</td>
-                    </tr>
-                    <tr>
-                        <td>Email Address</td>
-                        <td>{user.email}</td>
-                    </tr>
-                    <tr>
-                        <td>Password</td>
-                        <td>********</td>
-                    </tr>
-                </tbody>
-            </table>
-            <button><Link to={`/profile/${user.id}/edit`}>Edit Profile</Link></button>
-            <button><Link to={`/profile/${user.id}/change-password`}>Change Password</Link></button>
-        </>
+        <main id="profile">
+            <section>
+                <h1>Profile Page for {user.first_name} {user.last_name}</h1>
+                {message ? <div id="message">{message}</div> : null}
+                <div className="details">
+                    <span>Name: </span>
+                    <span>{user.first_name} {user.last_name}</span>
+                </div>
+                <div className="details">
+                    <span>Email address: </span>
+                    <span>{user.email}</span>
+                </div>
+                <div className="details">
+                    <span>Password: </span>
+                    <span>********</span>
+                </div>
+                <div className="buttons">
+                    <button><Link to={`/profile/${user.id}/edit`}>Edit Profile</Link></button>
+                    <button><Link to={`/profile/${user.id}/change-password`}>Change Password</Link></button>
+                </div>
+            </section>
+        </main>
     )
 };
 
