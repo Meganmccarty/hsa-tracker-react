d47e2f6f914f7f0333a6ccfa70e405cebab40d51
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-09 13:27:49 -0400

    Import module css styles, tweak form

--- src/forms/passwords/ForgotPassword.js
+++ src/forms/passwords/ForgotPassword.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { userActions } from '../../store/userSlice';
 
-import '../user-forms.css';
+import styles from '../auth-forms.module.css';
 
 function ForgotPassword() {
     const dispatch = useDispatch();
@@ -42,22 +42,22 @@ function ForgotPassword() {
     }, [dispatch])
 
     return (
-        <main>
-            <section className="user-form" id="forgot-password">
-                <h1>Forgot Password</h1>
-                {message ? <div id="message">{message}</div> : null}
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="email"
-                        name="email"
-                        aria-label="Email address"
-                        onChange={handleEmailChange}
-                        placeholder="Email address"
-                    />
-                    <input type="submit" aria-label="Submit button" />
-                </form>
-            </section>
-        </main>
+        <section className={styles.authForm}>
+            <h1>Forgot Password</h1>
+            {message ? <div className={styles.message}>{message}</div> : null}
+            <p>Enter the email address you used when creating your account</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    aria-label="Email address"
+                    onChange={handleEmailChange}
+                    placeholder="Email address"
+                    required={true}
+                />
+                <input type="submit" aria-label="Submit button" />
+            </form>
+        </section>
     )
 };
 
