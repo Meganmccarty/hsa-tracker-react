b3ff3d48b7edece97c5316643e6d5c8875bc13fe
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-10 14:42:56 -0400

    Add text and images to home page, add styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,41 @@
+import { Link } from 'react-router-dom';
+
+import newReceipt from '../images/new-receipt.png';
+import receiptTable from '../images/receipt-table.png';
+import receiptDetail from '../images/receipt-detail.png';
+import receiptImage from '../images/receipt-image.png';
+import totalExpenses from '../images/total-expenses.png';
+import styles from './Home.module.css';
+
 function Home() {
-    return <h1>Welcome to HSA Tracker!</h1>
+    return (
+        <div className={styles.home}>
+            <h1>Welcome to HSA Tracker!</h1>
+            <h2>The app for tracking your Health Savings Account receipts!</h2>
+            <section className={styles.details}>
+                <img src={newReceipt} alt="New Receipt Form" />
+                <h3>Easily add a new receipt record and attach associated images!</h3>
+            </section>
+            <section className={styles.details}>
+                <h3>See all of your receipt records in a sortable, searchable table. You can also filter the table by year!</h3>
+                <img src={receiptTable} alt="Receipt Records Table" />
+            </section>
+            <section className={styles.details}>
+                <img src={receiptDetail} alt="Detailed Receipt Record" />
+                <h3>View the details of an individual receipt record, with options to edit or delete the record!</h3>
+            </section>
+            <section className={styles.details}>
+                <h3>View your attached receipt images. You can add as many images as you'd like for each receipt record!</h3>
+                <img src={receiptImage} alt="Receipt Image" />
+            </section>
+            <section className={styles.details}>
+                <img src={totalExpenses} alt="Total Expenses" />
+                <h3>Make filing taxes easy by viewing all your expenses for the year!</h3>
+            </section>
+            <h2 className={styles.signup}>Ready to get started?</h2>
+            <Link className={styles.signupLink} to="/signup">Create an account!</Link>
+        </div>
+    )
 };
 
 export default Home;
