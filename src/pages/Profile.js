24868f700bc56c2c2756685ba8a8aebcf1afb341
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-10 10:53:38 -0400

    Add module css

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { userActions } from '../store/userSlice';
 
-import './Profile.css';
+import styles from './Profile.module.css';
 
 function Profile() {
     const dispatch = useDispatch();
@@ -17,28 +17,26 @@ function Profile() {
     }, [dispatch]);
 
     return (
-        <main id="profile">
-            <section>
-                <h1>Profile Page for {user.first_name} {user.last_name}</h1>
-                {message ? <div id="message">{message}</div> : null}
-                <div className="details">
-                    <span>Name: </span>
-                    <span>{user.first_name} {user.last_name}</span>
-                </div>
-                <div className="details">
-                    <span>Email address: </span>
-                    <span>{user.email}</span>
-                </div>
-                <div className="details">
-                    <span>Password: </span>
-                    <span>********</span>
-                </div>
-                <div className="buttons">
-                    <Link className="blue button" to={`/profile/${user.id}/edit`}>Edit Profile</Link>
-                    <Link className="blue button" to={`/profile/${user.id}/change-password`}>Change Password</Link>
-                </div>
-            </section>
-        </main>
+        <section className={styles.profile}>
+            <h1>Profile Page for {user.first_name} {user.last_name}</h1>
+            {message ? <div className={styles.message}>{message}</div> : null}
+            <div className={styles.details}>
+                <span className={styles.bold}>Name: </span>
+                <span>{user.first_name} {user.last_name}</span>
+            </div>
+            <div className={styles.details}>
+                <span className={styles.bold}>Email address: </span>
+                <span>{user.email}</span>
+            </div>
+            <div className={styles.details}>
+                <span className={styles.bold}>Password: </span>
+                <span>********</span>
+            </div>
+            <div className={styles.buttons}>
+                <Link className={styles.blue} to={`/profile/${user.id}/edit`}>Edit Profile</Link>
+                <Link className={styles.blue} to={`/profile/${user.id}/change-password`}>Change Password</Link>
+            </div>
+        </section>
     )
 };
 
